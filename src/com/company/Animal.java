5ad8198dbe2b97317ec9a5b119cf2d867d58d52c
karolinasubstych <karolaj95@gmail.com>
2022-04-28 21:20:58 +0200

    Task 1 - Feed animal and Take For A Walk

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,28 @@
 package com.company;
 
 public class Animal {
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    private Double weight;
+    final String species = "Koń";
+
+    protected void feed() {
+        if (this.weight > 0)
+            this.weight += 0.1;
+        else
+            System.out.println(this.species + " nie żyje!");
+    }
+
+    protected void takeForAWalk() {
+        if (this.weight > 0)
+            this.weight -= 0.1;
+        else
+            System.out.println(this.species + " nie żyje!");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Human human = new Human();
+        human.pet.setWeight(500d);
+        human.pet.getWeight();
+        human.pet.takeForAWalk();
+        human.pet.takeForAWalk();
+        human.pet.takeForAWalk();
+        human.pet.feed();
+        human.pet.feed();
+        human.pet.feed();
+        human.pet.getWeight();
     }
 }
