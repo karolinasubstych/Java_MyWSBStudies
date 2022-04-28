d7d3dc6d2a9a3c6ab5a45c037975056367a7975e
karolinasubstych <karolaj95@gmail.com>
2022-04-28 21:26:20 +0200

    Task 1 - add weight printing out

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,17 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
         Human human = new Human();
         human.pet.setWeight(500d);
-        human.pet.getWeight();
+        System.out.println(human.pet.getWeight());
         human.pet.takeForAWalk();
         human.pet.takeForAWalk();
         human.pet.takeForAWalk();
         human.pet.feed();
         human.pet.feed();
         human.pet.feed();
-        human.pet.getWeight();
+        human.pet.feed();
+        System.out.println(human.pet.getWeight());
     }
 }
